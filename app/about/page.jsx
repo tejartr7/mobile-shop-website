bd4191dde85c9dd3761dce962d9ab40dd357c8c9
tejartr7 <raghusai126@gmail.com>
2023-12-20 19:11:51 +0530

    rtr

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -8,16 +8,13 @@ export default function About() {
                 {/* Content of the About section goes here */}
 
                 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4'>
-                    <div className='col-span-2 lg:p-4 text-center text-xl' style={{color:'#333333'}}>
+                    <div className='col-span-2 lg:p-4 text-center text-xl flex flex-col items-center justify-center' style={{ color: '#333333' }}>
                         <h2 className='text-3xl my-2' style={{ color: '#ff8c00' }}>Welcome To</h2>
-                        <h1 className='text-5xl my-2' style={{ color: '#333333' }}>Bihar Ka Swaad</h1>
-                        <p>
-                           lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
+                        <h1 className='text-5xl my-2' style={{ color: '#333333' }}>Swaad Bihar ka</h1>
+                        <p className='text-xl font-bold' style={{ color: '#333333' }}>
+                            Swaad Bihar ka is a cloud kitchen and food delivery service in Patna, Bihar. We specialize in delivering the best and most delicious food in Bihar at the most affordable prices. Our dedication to quality and taste sets us apart. Choose from our extensive range of food items to satisfy your cravings. Experience the convenience of ordering from us through Zomato and Swiggy, and let us bring the authentic flavors of the best food to your doorstep.
                         </p>
-
-                       
                     </div>
-
                     <div className='md:col-span-2 lg:col-span-2 flex items-center justify-center my-8'>
                         <Image
                             src={logo}
@@ -29,7 +26,7 @@ export default function About() {
                             objectPosition="center center"
                             className='about-image rounded-full mx-auto md:w-500 md:h-500 lg:w-96 lg:h-96 xl:w-120 xl:h-120'  // Add rounded styling if needed
                         />
-                       
+
                     </div>
                 </div>
             </div>
--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -1,38 +1,39 @@
 import { BiSolidPhoneCall } from "react-icons/bi";
 import { FaGlobe, FaInstagramSquare } from "react-icons/fa";
 import { IoIosMail } from "react-icons/io";
+import { SiZomato } from "react-icons/si";
+
 export default function Contact() {
     return (
         <section id="contact">
-            <div class="font-Merriweather max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
-
-                <div class="flex justify-center mt-8 space-x-6">
+            <div className="font-Merriweather max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8">
+                <div className="flex justify-center mt-8 space-x-6">
                     <div className="link-wrapper">
                         <a target="_blank" href="tel:+917044206013">
                             <span className="sr-only">Call</span>
-                            <BiSolidPhoneCall size={25} />
+                            <BiSolidPhoneCall size={40} />
                         </a>
                     </div>
                     <div className="link-wrapper">
                         <a target="_blank" href="https://link.zomato.com/xqzv/rshare?id=442892287f0b9858">
                             <span className="sr-only">Zomato</span>
-                            <FaGlobe size={25} />
+                            <SiZomato size={50} className="m-0 p-0" />
                         </a>
                     </div>
                     <div className="link-wrapper">
                         <a target="_blank" href="mailto:swwaadbiharka@gmail.com">
                             <span className="sr-only">Mail</span>
-                            <IoIosMail size={30} />
+                            <IoIosMail size={40} />
                         </a>
                     </div>
                     <div className="link-wrapper">
                         <a target="_blank" href="https://www.instagram.com/biharkaswaad/">
                             <span className="sr-only">Instagram</span>
-                            < FaInstagramSquare size={25} />
+                            <FaInstagramSquare size={40} />
                         </a>
                     </div>
                 </div>
-                <p class="mt-8 text-base leading-6 text-center text-2xl">
+                <p className="mt-8 text-base leading-6 text-center text-2xl">
                     © Design and developed by <a target="_blank" href="https://twitter.com/raghu_rtr" style={{ color: '#ff8c00' }}>RTR</a>
                 </p>
             </div>
