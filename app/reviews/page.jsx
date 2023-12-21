328ff6824d3a3a69004cce544f85cbcc45acc4e3
tejartr7 <raghusai126@gmail.com>
2023-12-21 11:57:56 +0530

    swaad thursday

--- app/reviews/page.jsx
+++ app/reviews/page.jsx
@@ -4,10 +4,14 @@ import 'swiper/css';
 import 'swiper/css/navigation';
 import Image from 'next/image';
 import reviewData from './reviews';
-import logo from '../images/logo.png';
+import profile from '../images/profile.png';
 import { useState, useEffect } from 'react';
 import SwiperCore, { Navigation, Pagination, Autoplay } from 'swiper';
 SwiperCore.use([Navigation, Pagination, Autoplay]);
+import StarComponent1 from './Star';
+import StarComponent2 from './Star2';
+import logo from '../images/logo.png';
+import rating_1 from '../images/rating_1.jpeg';
 const ReviewSlider = () => {
     const [swiper, setSwiper] = useState(null);
 
@@ -32,7 +36,7 @@ const ReviewSlider = () => {
 
     return (
         <section id="reviews" className="swiper-container">
-            <h2 className="text-5xl font-semibold mb-4 text-center" style={{color:'#333333'}}>Customer Reviews</h2>
+            <h2 className="text-5xl font-semibold mb-4 text-center" style={{ color: '#333333' }}>Customer Reviews</h2>
             <Swiper
                 slidesPerView={1}
                 navigation={{ prevEl: '.swiper-button-prev', nextEl: '.swiper-button-next' }} // Add this line for navigation
@@ -51,17 +55,50 @@ const ReviewSlider = () => {
                 }}
             >
                 {reviewData.map((review) => (
-                    <SwiperSlide key={review.id}>
-                        <div className="review-card bg-gray-600 text-white p-6 rounded-lg text-center flex flex-col items-center m-2">
-                            <Image src={logo} alt={review.name} width={50} height={50} className="mb-4" />
+                    <SwiperSlide key={review.id} style={{ height: '100%' }}>
+                        <div className="review-card bg-gray-600 text-white p-6 rounded-lg text-center flex flex-col items-center m-2" style={{ height: '100%' }}>
+                            <Image src={profile} alt={review.name} width={50} height={50} className="mb-4" />
                             <h3 className="text-xl font-semibold mb-2">{review.name}</h3>
-                            <p className="mb-2">Rating: {review.rating}</p>
+                            <p className="mb-2">Rating: {Number(review.rating) === 5 ? <StarComponent2 rating={review.rating} /> : <StarComponent1 rating={review.rating} />}</p>
                             <p>{review.comments}</p>
                         </div>
                     </SwiperSlide>
                 ))}
             </Swiper>
-            
+            <div>
+                <div className='font-Merriweather gap-4 mx-4 lg:mx-40 mt-6 mb-4 lg:mb-8'>
+                    {/* Content of the About section goes here */}
+
+                    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4'>
+                        <div className='md:col-span-2 lg:col-span-2 flex flex-col items-center justify-center my-8'>
+                            <h2 className='text-center mb-4 text-3xl font-bold' style={{ color: '#333333' }} > <u>Siggy Rating</u></h2>
+                            <Image
+                                src={rating_1}
+                                width={400}
+                                height={500}
+                                alt="About Image"
+                                layout="fixed"
+                                objectFit="cover"
+                                objectPosition="center center"
+                                className='about-image rounded-full mx-auto sm:h-400 md:w-500 md:h-500 lg:w-96 lg:h-96 xl:w-120 xl:h-120'
+                            />
+                        </div>
+                        <div className='md:col-span-2 lg:col-span-2 flex flex-col items-center justify-center my-8'>
+                            <h2 className='text-center mb-4 text-3xl font-bold' style={{ color: '#333333' }} ><u>Zomato Rating</u></h2>
+                            <Image
+                                src={rating_1}
+                                width={400}
+                                height={500}
+                                alt="About Image"
+                                layout="fixed"
+                                objectFit="cover"
+                                objectPosition="center center"
+                                className='about-image rounded-full mx-auto sm:h-400 md:w-500 md:h-500 lg:w-96 lg:h-96 xl:w-120 xl:h-120'
+                            />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </section>
     );
 };
--- app/reviews/reviews.js
+++ app/reviews/reviews.js
@@ -1,39 +1,39 @@
 const reviewData = [
     {
         id: 1,
-        name: 'Bihari Thali',
-        rating: 4.5,
-        comments: 'The food was delicious and the delivery was on time. I would definitely recommend this place to my friends and family.',
+        name: 'Ajay',
+        rating: 5,
+        comments: 'The quality and taste of the Litti was very superb.Packaging as great.It made my day.Thanks.Will order again.It was a great experience.🙌',
     },
     {
         id: 2,
-        name: 'Chicken Biryani',
-        rating: 4.5,
-        comments: 'The food was delicious and the delivery was on time. I would definitely recommend this place to my friends and family.',
+        name: 'Pallavi',
+        rating: 5,
+        comments: "Their samosas are so light and tasty,since I started ordering from here I don't eat somosa from anywhere,they are so good.I love themm 😍"
     },
     {
         id: 3,
-        name: 'Chicken Biryani',
-        rating: 4.5,
-        comments: 'The food was delicious and the delivery was on time. I would definitely recommend this place to my friends and family.',
+        name: 'Akshay',
+        rating: 4,
+        comments: 'The food was delicious and the delivery was on time. Liked the taste and quantity.would love to order again from them soon 😊',
     },
     {
         id: 4,
-        name: 'Chicken Biryani',
-        rating: 4.5,
-        comments: 'The food was delicious and the delivery was on time. I would definitely recommend this place to my friends and family.',
+        name: 'Shashi',
+        rating: 5,
+        comments: 'Everytime I want to have something tasty and homely food I order from your restaurant. The food is always tasty and never disappoints me 😊💖',
     },
     {
         id: 5,
-        name: 'Chicken Biryani',
-        rating: 4.5,
-        comments: 'The food was delicious and the delivery was on time. I would definitely recommend this place to my friends and family.',
+        name: 'Nitu',
+        rating: 5,
+        comments: 'The food was very tasty, especially for the portion size and the price. The delivery and packaging was good. Happy with the service. 😊👍',
     },
     {
         id: 6,
-        name: 'Chicken Biryani',
-        rating: 4.5,
-        comments: 'The food was delicious and the delivery was on time. I would definitely recommend this place to my friends and family.',
+        name: 'Suraj',
+        rating: 5,
+        comments: 'Just loved the food like it didnt felt not even 1% that I have ordered something from a restaurant,gave full home made vibes✨✨.',
     },
 ];
 export default reviewData;
